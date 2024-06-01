0665fd202dc3e86ff0a9c45077216e4e7e03388d
Stephen Bunn <stephen@bunn.io>
2024-06-01 08:12:33 -0400

    Adding catppucin theme for fzf
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- fish/fzf.fish
+++ fish/fzf.fish
@@ -1,1 +1,4 @@
-set -gx FZF_DEFAULT_OPTS "--color 'fg:{{dracula_foreground}},fg+:{{dracula_purple}},bg+:{{dracula_current_line}},hl:{{dracula_green}},info:{{dracula_comment}}'"
+set -gx FZF_DEFAULT_OPTS "\
+--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+--color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
